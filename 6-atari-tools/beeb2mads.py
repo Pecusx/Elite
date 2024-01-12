af2611df4ab59954c727e90a104c2e2e3dff0319
pkali <pawel.kalinowski@gmail.com>
2024-01-11 20:10:47 -0500

    3rd try

--- 6-atari-tools/beeb2mads.py
+++ 6-atari-tools/beeb2mads.py
@@ -1,19 +1,24 @@
 import re
 import sys
-
 replacements = {
+    r'P%': r'*',  # P% to * -- P% A special symbol which returns the current address being assembled
     r'^\\': ';',  # Replace \ with ; (at the beginning of the line)
     r'\\ ': '; ',  # later in line.
     r'\\$': ';',   # Replace \ with ; (at the end of the line)
+    r'^.': '',  # remove the dot from label definitions (MIGHT BREAK OTHER STUFF)
+    r'([A-Za-z]+)%': r'\1__',  # var ending % replaced with __ (not used in original source)
     r'&([0-9A-Fa-f]+)': r'$\1',  # &BACA to $BACA
     r'SKIP (\d+)': r' .ds \1',  # SKIP 4 to .ds 4
-    r'P%': r'*',  # P% to * -- P% A special symbol which returns the current address being assembled
+    r'EQUB ': ' .by ',  # bytes
+    r'CHAR ': ' .by ',  # chars
+    r'EQUW ': ' .wo ',  # words
     # r'(?<=PRINT.*)(\~)': ''  # removes ~ from PRINT statements (~ means display in HEX)
     # the above regex does not work in python re lib (no lookbehind for variable length pattern)
     r'PRINT(.*)\~(.*)': r'PRINT\1\2',  # remove 1 tilde from a line w/ PRINT
     r'PRIN(.*)\~(.*)': r'PRIN\1\2',  # remove the second tilde (haxxx)
 
 }
+
 
 
 def apply_replacements(text, replacements):
